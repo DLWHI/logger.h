d46c9c1fa48e8904383dd2d0815f11da90049861
Steve <samimmortal999@gmail.com>
2025-07-13 15:41:34 +0300

    first commit

---
+++ include/logger.h
@@ -0,0 +1,48 @@
+#ifndef LOGGER_H_LOGGER_H_
+#define LOGGER_H_LOGGER_H_
+
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+namespace logh {
+  static std::ostream& log_stream = std::cout;
+
+  enum class log_level {
+    notify,
+    warning,
+    error
+  };
+
+  std::ostream& operator<<(std::ostream& os, log_level lvl) {
+    switch (lvl) {
+      case log_level::notify:
+        return os << "NOTIFY";
+      case log_level::warning:
+        return os << "WARNING";
+      case log_level::error:
+        return os << "ERROR";
+      default:
+        throw;
+    }
+  }
+
+
+  void log(log_level lvl, const char* msg) {
+    auto timestamp = std::chrono::system_clock::now();
+    log_stream_ << '[' << std::ctime(&timestamp) << ']' << lvl << '|' << msg << std::endl;
+  }
+
+  void log(log_level lvl, const std::string& msg) {
+    log(lvl, msg.c_str());
+  }
+
+  void log(log_level lvl, const std::string_view& msg) {
+    auto timestamp = std::chrono::system_clock::now();
+    log_stream_ << '[' << std::ctime(&timestamp) << ']' << lvl << '|' << msg << std::endl;
+  }
+}
+
+#endif  // LOGGER_H_LOGGER_H
