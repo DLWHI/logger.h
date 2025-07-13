b5067f046173884e89446fb45e7c37a4d71eff63
Steve <samimmortal999@gmail.com>
2025-07-13 17:17:24 +0300

    fixed \n at the end of timestamp

--- include/logger/logger.h
+++ include/logger/logger.h
@@ -1,6 +1,7 @@
 #ifndef LOGGER_H_LOGH_LOGGER_H_
 #define LOGGER_H_LOGH_LOGGER_H_
 
+#include <chrono>
 #include <ctime>
 #include <iostream>
 #include <string>
@@ -8,22 +9,26 @@
 
 namespace logger {
   static std::ostream& log_stream = std::cout;
+  static char time_buf[80] = {0};
 
   std::ostream& message() {
     std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    log_stream << '[' << std::ctime(&timestamp) << ']' << "MESSAGE" << '|';
+    std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]");  
+    log_stream << time_buf << "MESSAGE" << '|';
     return log_stream;
   }
 
   std::ostream& warning() {
     std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    log_stream << '[' << std::ctime(&timestamp) << ']' << "WARNING" << '|';
+    std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]"); 
+    log_stream << time_buf << "WARNING" << '|';
     return log_stream;
   }
 
   std::ostream& error() {
-    std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    log_stream << '[' << std::ctime(&timestamp) << ']' << "ERROR" << '|';
+   std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]");
+    log_stream << time_buf << "ERROR" << '|';
     return log_stream;
   }
 }
