19a8e53468f4bff7fad7269f0d6fbbc139ea3ea9
Steve <samimmortal999@gmail.com>
2025-07-13 18:00:25 +0300

    fixed strftime

--- include/logger/logger.h
+++ include/logger/logger.h
@@ -13,21 +13,21 @@ namespace logger {
 
   std::ostream& message() {
     std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]");  
+    std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]", std::localtime(&timestamp));  
     log_stream << time_buf << "MESSAGE" << '|';
     return log_stream;
   }
 
   std::ostream& warning() {
     std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]"); 
+    std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]", std::localtime(&timestamp)); 
     log_stream << time_buf << "WARNING" << '|';
     return log_stream;
   }
 
   std::ostream& error() {
    std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]");
+    std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]", std::localtime(&timestamp));
     log_stream << time_buf << "ERROR" << '|';
     return log_stream;
   }
