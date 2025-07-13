4e1bfeff9a8c5054bc1a7f0ce33b39705ac0f892
Steve <samimmortal999@gmail.com>
2025-07-13 16:39:12 +0300

    compilation fix

--- include/logger/logger.h
+++ include/logger/logger.h
@@ -1,7 +1,6 @@
 #ifndef LOGGER_H_LOGH_LOGGER_H_
 #define LOGGER_H_LOGH_LOGGER_H_
 
-#include <chrono>
 #include <ctime>
 #include <iostream>
 #include <string>
@@ -11,20 +10,20 @@ namespace logger {
   static std::ostream& log_stream = std::cout;
 
   std::ostream& message() {
-    auto timestamp = std::chrono::system_clock::now();
-    log_stream_ << '[' << std::ctime(&timestamp) << ']' << "MESSAGE" << '|';
+    std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    log_stream << '[' << std::ctime(&timestamp) << ']' << "MESSAGE" << '|';
     return log_stream;
   }
 
   std::ostream& warning() {
-    auto timestamp = std::chrono::system_clock::now();
-    log_stream_ << '[' << std::ctime(&timestamp) << ']' << "WARNING" << '|';
+    std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    log_stream << '[' << std::ctime(&timestamp) << ']' << "WARNING" << '|';
     return log_stream;
   }
 
   std::ostream& error() {
-    auto timestamp = std::chrono::system_clock::now();
-    log_stream_ << '[' << std::ctime(&timestamp) << ']' << "ERROR" << '|';
+    std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    log_stream << '[' << std::ctime(&timestamp) << ']' << "ERROR" << '|';
     return log_stream;
   }
 }
