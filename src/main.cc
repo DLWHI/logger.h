f6eb5d0381768a023140d6e2257779fc76173fef
Mukhammet Rakhymov <samimmortal999@gmail.com>
2025-07-13 14:47:35 +0300

    Initial commit

---
+++ src/main.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Keep your heart open" << std::endl;
+  std::cout << "So you could see the first star in the clear sky" << std::endl;
+  return 0;
+}
---
+++ tests/phony_test.cc
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+TEST(PhonyTest, test_one) {
+  std::cout << "Guess it maybe a start to something great..." << std::endl;
+}
