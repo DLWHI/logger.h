1eaa973bc11f926a3c7dd918876818c873241861
Steve <samimmortal999@gmail.com>
2025-07-18 08:32:47 +0300

    changed fucntions to be static

--- include/logger/logger.h
+++ include/logger/logger.h
@@ -11,21 +11,21 @@ namespace logger {
   static std::ostream& log_stream = std::cout;
   static char time_buf[80] = {0};
 
-  std::ostream& message() {
+  static std::ostream& message() {
     std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]", std::localtime(&timestamp));  
     log_stream << time_buf << "MESSAGE" << '|';
     return log_stream;
   }
 
-  std::ostream& warning() {
+  static std::ostream& warning() {
     std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]", std::localtime(&timestamp)); 
     log_stream << time_buf << "WARNING" << '|';
     return log_stream;
   }
 
-  std::ostream& error() {
+  static std::ostream& error() {
    std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]", std::localtime(&timestamp));
     log_stream << time_buf << "ERROR" << '|';
