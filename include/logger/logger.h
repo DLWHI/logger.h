827625441e1180fd891bbea5cb46ada02555cfb1
Steve <samimmortal999@gmail.com>
2025-08-08 12:00:01 +0300

    it went too far

--- include/logger/logger.h
+++ include/logger/logger.h
@@ -9,36 +9,37 @@
 
 class logger {
  private:
-  inline static std::ostream* log_stream;
-  inline static char time_buf[25];
+  static std::ostream*& log_stream() noexcept {
+    static std::ostream* lgs = &std::cout;
+    return lgs;
+  }
+  
+  static char time_buf[25];
  
  public:
   static std::ostream& message() {
     std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]", std::localtime(&timestamp));  
-    *log_stream << time_buf << "MESSAGE" << '|';
+    *log_stream() << time_buf << "MESSAGE" << '|';
     return *log_stream;
   }
 
   static std::ostream& warning() {
     std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]", std::localtime(&timestamp)); 
-    *log_stream << time_buf << "WARNING" << '|';
+    *log_stream() << time_buf << "WARNING" << '|';
     return *log_stream;
   }
 
   static std::ostream& error() {
    std::time_t timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     std::strftime(time_buf, sizeof(time_buf), "[%a %b %d %Y %H:%M:%S]", std::localtime(&timestamp));
-    *log_stream << time_buf << "ERROR" << '|';
+    *log_stream() << time_buf << "ERROR" << '|';
     return *log_stream;
   }
 
-  static void set_log_stream(std::ostream& stream) { log_stream = &stream; }
+  static void set_log_stream(std::ostream& stream) { log_stream() = &stream; }
 };
-
-std::ostream* logger::log_stream = &std::cout;
-char logger::time_buf[25] = {0};
 
 #endif  // LOGGER_H_LOGGER_LOGGER_H_
 
